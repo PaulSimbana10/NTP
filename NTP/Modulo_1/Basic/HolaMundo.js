f2eaba9753f531a4f1e26a3607777b5800089a41
Paul Simbaña <dennis-paul10@outlook.es>
2021-06-18 12:41:25 -0500

    Consulta
    consulta console

--- NTP/Modulo_1/Basic/HolaMundo.js
+++ NTP/Modulo_1/Basic/HolaMundo.js
@@ -1,1 +1,47 @@
 console.log("hola mundo");
+
+//Métodos
+
+//Consulta
+// Console.assert()
+// Registra un mensaje y envía una traza de error a la consola si el primer argumento es false.
+// Console.count()
+// Registra el numero de veces que esta línea ha sido llamada con etiqueta dada.
+// Console.debug() (en-US) 
+// Registra un mensaje con el nivel de"debug".
+// Note: A partir de Chromium 58 este método solo aparece en las consolas de navegadores Chromium cuando el nivel "Verbose" está seleccionado.
+// Console.dir()
+// Muestra un listado interactivo de las propiedades de un objeto JavaScript específico. Esta lista le permite examinar el contenido de los objetos hijos.
+// Console.dirxml()
+// Muestra una representación en forma de árbol de un elemento XML/HTML si es posible o la vista del objeto JavaScript si no es posible.
+
+// Console.error()
+// Muestra un mensaje de error. Se pueden utilizar sustituciones de cadena y argumentos adicionales con este método.
+// Console._exception()
+// Es un alias de error().
+// Console.group() (en-US)
+// Crea un nuevo grupo, indentando todos los mensajes subsecuentes en un nuevo nivel. Para retroceder un nivel, se utiliza groupEnd().
+// Console.groupCollapsed() (en-US)
+// Crea un nuevo grupo, indentando todos los mensajes subsecuentes en un nuevo nivel. A diferencia de group(), inicia con la línea de grupo colapsada, requiriendo el uso de un botón de apertura para expandir el grupo. Para retroceder un nivel, se utiliza groupEnd().
+// Console.groupEnd() (en-US)
+// Finaliza el grupo actual.
+// Console.info()
+// Muestra un mensaje de información en la Consola Web. Se puede utilizar sustitución de cadena y argumentos adicionales con este método.
+// Console.log()
+// Para mensajes generales de registro de información. Se puede utilizar sustitución de cadena y argumentos adicionales con este método.
+// Console.profile() (en-US)
+// Inicia el registro de un perfil de rendimiento (Firefox performance tool). Se puede especificar un nombre opcional para el perfil.
+// Console.profileEnd() (en-US)
+// Detiene el perfil. Usted puede revisar el perfil resultante en la herramienta de desempeño del navegador (por ejemplo, la herramienta de rendimiento de Firefox).
+// Console.table()
+// Muestra datos tabulares en forma de una tabla.
+// Console.time()
+// Inicia un temporizador con un nombre específico como parámetro. Hasta 10.000 temporizadores simultáneos pueden ejecutarse en una página determinada.
+// Console.timeEnd()
+// Detiene el temporizador especificado y registra el tiempo transcurrido, en segundos, desde su inicio.
+// Console.timeStamp() (en-US)
+// Adiciona una marca a las herramientas Timeline o Waterfall del navegador.
+// Console.trace()
+// Muestra una traza de error.
+// Console.warn()
+// Muestra un mensaje de advertencia. Se puede utilizar sustitución de cadena y argumentos adicionales con este método.
