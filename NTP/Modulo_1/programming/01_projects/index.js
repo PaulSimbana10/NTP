2bc6bd9fce3beef0701b29aea5bf2f1fd299bdbd
Paul Simbaña <dennis-paul10@outlook.es>
2021-06-24 12:12:07 -0500

    actividad_clase_24/06/2021

--- NTP/Modulo_1/programming/01_projects/index.js
+++ NTP/Modulo_1/programming/01_projects/index.js
@@ -7,7 +7,34 @@ app.get('/', (req, res)=>{
     res.send("Hola mundo con ExpresJs")
 
 
+});
+
+app.get("/saludo",(req,res)=>{
+    //req.query
+    // console.log(req.query);
+    //obtenemos la query del objeto
+    const { 
+        query: {nombre, apellido} 
+    }= req;
+
+    //const nombre = req.query.nombre
+    //const apellido = rec.query.nombre
+
+    res.json({
+        saludo: `hola soy ${nombre} ${apellido}`,
+    });
 })
+
+app.get('/saludo/:nombre', (req, res)=>{
+    const { params: {nombre} } = req
+    //console.log(req.params)
+    // const nombre = req.params.nombre
+
+    res.json({
+       nombre: nombre
+   });
+});
+
 
 app.listen(3000, ()=>{
     console.log("servidor escuchando en http://localhost:3000");
---
+++ NTP/Modulo_1/programming/01_projects/routes/index.js
@@ -0,0 +1,18 @@
+//traemos el paquete
+const {Router} = require('express');
+
+//iniciamos router
+const router = Router()
+
+//correccion de los endpoint
+
+
+//crear un nuevo archivo de rutas
+module.exports.RouterIndex = router
+
+// module = {
+//     exports: {
+//         RouterIndex: router
+//     }
+// }
+
